814beebaab00b3a728966405547807bc0baaab81
nameyq <34115445+nameyq@users.noreply.github.com>
2021-03-14 11:55:59 +0800

    Update WPS_accept_invitation.py

--- WPS_accept_invitation.py
+++ WPS_accept_invitation.py
@@ -26,7 +26,7 @@ def request_re(sid, invite_userid, rep = 33):
     js = json.loads(r.content)
     if js['msg'] == 'tryLater' and rep > 0:
         rep -= 1
-        time.sleep(5)
+        time.sleep(10)
         r = request_re(sid, invite_userid, rep)
     return r
 
